bc6965b06ec7fa2841c104df6e1e69af66ef366f
Lucas Cheon <inwoo@inwoo-mbp.local>
2016-08-03 16:26:02 -0700

    m

--- example.js
+++ example.js
@@ -1,45 +1,40 @@
 var ms;
 var sb;
 var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-var queue = [];
+var queue = [
+  'fftest/10154354781949643-init.m4v',
+  'fftest/10154354781949643-7894420.m4v',
+  'fftest/10154354781949643-7895421.m4v',
+  'fftest/10154354781949643-7896422.m4v',
+];
  
 function example() {
   ms = new MediaSource;
   var video = document.querySelector('video');
   video.src = URL.createObjectURL(ms);
-  ms.addEventListener('sourceopen', function() {
-    video.play();
-    console.log('open');
-    sb = ms.addSourceBuffer(mimeCodec);
-    fetch('fftest/10154354781949643-init.m4v');
-    fetch('fftest/10154354781949643-7894420.m4v');
-    // fetch('fftest/10154354781949643-7895421.m4v');
-    // fetch('fftest/10154354781949643-7896422.m4v');
-  }, false);
+  ms.addEventListener('sourceopen', sourceOpen);
 }
 
-function fetch(url) {
+function sourceOpen(_) {
+  console.log('sourceopen');
+  sb = ms.addSourceBuffer(mimeCodec);
+  fetch(queue.shift(), function(buf) {
+    sb.addEventListener('updateend', function(_) {
+      ms.endOfStream();
+      video.play();
+    });
+    console.log('appendbuffer');
+    sb.appendBuffer(buf);
+  });
+}
+
+function fetch(url, cb) {
   var xhr = new XMLHttpRequest;
   xhr.open('get', url);
   xhr.responseType = 'arraybuffer';
   xhr.onload = function() {
-    console.log('push');
-    if (ms.readyState === 'open') {
-      console.log(xhr.response, xhr.response.byteLength);
-      try {
-        sb.appendBuffer(xhr.response);
-      } catch (e) {
-        console.log('error', e);
-      }
-      sb.addEventListener('updateend', function() {
-        console.log(sb.buffered);
-      });
-    } else {
-      ms.addEventListener('sourceopen', function() {
-        console.log('openagain');
-      });
-    }
-  };
+    cb(xhr.response);
+  }
   xhr.send();
 }
 
