cb32539b7cc5762fe1d54524827145b9c88daca5
Lucas Cheon <inwoo@inwoo-mbp.local>
2016-08-02 17:31:54 -0700

    te

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
 var ms;
+var sb;
 var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
  
 function example() {
- ms = new MediaSource;
+  ms = new MediaSource;
   var video = document.querySelector('video');
   video.src = URL.createObjectURL(ms);
   ms.addEventListener('sourceopen', function() {
+    sb = ms.addSourceBuffer(mimeCodec);
     fetch('fftest/10154354781949643-7894420.m4v');
     fetch('fftest/10154354781949643-7895421.m4v');
     fetch('fftest/10154354781949643-7896422.m4v');
@@ -17,7 +19,6 @@ function fetch(url) {
   xhr.open('get', url);
   xhr.responseType = 'arraybuffer';
   xhr.onload = function() {
-    var sb = ms.addSourceBuffer(mimeCodec);
     sb.appendBuffer(xhr.response);
     console.log(sb.buffered);
   };
