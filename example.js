79b42abe41483775c5ad561a10b93444b0c76130
Lucas Cheon <inwoo@inwoo-mbp.local>
2016-08-02 17:00:11 -0700

    te

--- example.js
+++ example.js
@@ -2,11 +2,21 @@ function example() {
   var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
   var ms = new MediaSource;
   var video = document.querySelector('video');
-  console.log(window.URL.createObjectURL(ms));
   video.src = URL.createObjectURL(ms);
   ms.addEventListener('sourceopen', function() {
     console.log('open');
+    fetch('fftest/10154354781949643-7894420.m4v');
   });
+}
+
+function fetch(url) {
+  var xhr = new XMLHttpRequest;
+  xhr.open('get', url);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function() {
+    console.log(xhr.response);
+  }
+  xhr.send();
 }
 
 window.onload = example;
