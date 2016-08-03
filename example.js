aab3120dde529423f9819b3cfba0920813781a77
Lucas Cheon <inwoo@inwoo-mbp.local>
2016-08-02 17:56:35 -0700

    te

--- example.js
+++ example.js
@@ -13,9 +13,11 @@ function example() {
     fetch('fftest/10154354781949643-7895421.m4v');
     fetch('fftest/10154354781949643-7896422.m4v');
     sb.addEventListener('updateend', function() {
-      debugger;
-      console.log('updateend', queue);
-      sb.appendBuffer(queue.shift());
+      if (ms.readyState === 'open') {
+        debugger;
+        console.log('updateend', queue);
+        sb.appendBuffer(queue.shift());
+      }
     });
   });
 }
